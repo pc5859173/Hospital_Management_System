0afcd300591fb23a93baf7384f172afb55e3297a
pc5859173 <pc5859173@gmail.com>
2023-01-15 18:34:09 +0530

    code

--- app.py
+++ app.py
@@ -3,19 +3,36 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
-
 # database connection
-app = app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://priya01:Priya#8789@pc-test.database.windows.net/hmsdb?driver=SQL+Server"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://priya01:Priya#8789@pc-test.database.windows.net/hmsdb?driver=SQL+Server"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class User:
-    uid = db.Column(db.Integer,primary_key = True)
+# user class
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
     username =  db.Column(db.String(100))  
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(1000))
+
+# patient class
+class Patients(db.Model):
+    pid = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(100))       
+    name = db.Column(db.String(100))
+    gender =db.Column(db.String(100)) 
+    slot = db.Column(db.String(100))
+    time =db.Column(db.String(100),nullable = "False") 
+    date = db.Column(db.String(100),nullable = "False")
+    disease =db.Column(db.String(100)) 
+    dept = db.Column(db.String(100))
+    number = db.Column(db.String(100))
+    
 
 @app.route('/')
 def index():
     return ("test page")
 
+if __name__ == '__main__':
+    app.run(debug=True)
