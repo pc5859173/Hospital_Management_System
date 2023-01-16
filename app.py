8240e1abfc4a99364c610cd39b70f89829ed69e0
pc5859173 <pc5859173@gmail.com>
2023-01-16 22:21:58 +0530

    tables

--- app.py
+++ app.py
@@ -17,56 +17,56 @@ class User(db.Model):
     password = db.Column(db.String(1000))
 
 # patient class
-class Patients(db.Model):
-    pid = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(100))       
-    name = db.Column(db.String(100))
-    gender =db.Column(db.String(100)) 
-    slot = db.Column(db.String(100))
-    time =db.Column(db.String(100),nullable = "False") 
-    date = db.Column(db.String(100),nullable = "False")
-    disease =db.Column(db.String(100)) 
-    dept = db.Column(db.String(100))
-    number = db.Column(db.String(100))
+# class Patients(db.Model):
+#     pid = db.Column(db.Integer, primary_key = True)
+#     email = db.Column(db.String(100))       
+#     name = db.Column(db.String(100))
+#     gender =db.Column(db.String(100)) 
+#     slot = db.Column(db.String(100))
+#     time =db.Column(db.String(100),nullable = "False") 
+#     date = db.Column(db.String(100),nullable = "False")
+#     disease =db.Column(db.String(100)) 
+#     dept = db.Column(db.String(100))
+#     number = db.Column(db.String(100))
 
 # Doctors Class
-class Doctors(db.Model):
-    did = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(1000))  
-    doctorname = db.Column(db.String(100))
-    dept = db.Column(db.String(100))    
+# class Doctors(db.Model):
+#     did = db.Column(db.Integer, primary_key = True)
+#     email = db.Column(db.String(1000))  
+#     doctorname = db.Column(db.String(100))
+#     dept = db.Column(db.String(100))    
     
 # here we will pass endpoints and run the function
-@app.route('/')
-def home():
-    return render_template("home.html")
+# @app.route('/')
+# def home():
+#     return render_template("home.html")
 
-@app.route('/doctors')
-def doctors():
-    return render_template("doctor.html")    
+# @app.route('/doctors')
+# def doctors():
+#     return render_template("doctor.html")    
 
-@app.route('/patients')
-def patients():
-    return render_template("patient.html") 
+# @app.route('/patients')
+# def patients():
+#     return render_template("patient.html") 
 
-@app.route('/bookings')
-def bookings():
-    return render_template("booking.html") 
+# @app.route('/bookings')
+# def bookings():
+#     return render_template("booking.html") 
 
-@app.route('/login')
-def login():
-    return render_template("login.html") 
+# @app.route('/login')
+# def login():
+#     return render_template("login.html") 
 
-@app.route('/signup',methods = ["POST","GET"])
-def signup():
-    if request.method=="POST":
-        print("This is post method")
-    print("This is get method")    
-    return render_template("signup.html") 
+# @app.route('/signup',methods = ["POST","GET"])
+# def signup():
+#     if request.method=="POST":
+#         print("This is post method")
+#     print("This is get method")    
+#     return render_template("signup.html") 
 
-@app.route('/logout')
-def logout():
-    return render_template("login.html")    
+# @app.route('/logout')
+# def logout():
+#     return render_template("login.html")    
 
 
 
