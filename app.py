acdf1714ebe27fa6d4e40becf145b8a64785f711
pc5859173 <pc5859173@gmail.com>
2023-01-19 15:50:17 +0530

    commit

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ class Doctors(db.Model):
 # route for home 
 @app.route('/')
 def home():
-    return render_template("admin.html")
+    return render_template("user_signup.html")
     # return render_template("index.html")
 
 # route for doctors 
